87c0a85c17f9430590f1eabb705cdf76060f582d
VRPP CO-ORDINATOR <RAMRAO@VRPPs-MacBook-Air.local>
2019-10-06 08:34:18 +0530

    Created first component

---
+++ src/components/Swagh.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Swagh = () => {
+  return (
+    <div>
+      <h1>Saheel Wagh</h1>
+      <h2>Favourite Langauge : Javascript</h2>
+    </div>
+  );
+};
+export default Swagh;
