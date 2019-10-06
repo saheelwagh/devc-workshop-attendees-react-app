35598a7eb0ad4033a2d838cef8d089d2e31520f1
Hrishikesh24 <rishi2001mahesh@mail.com>
2019-10-06 13:07:35 +0530

    Added a Hrishikesh.js (attendee)

---
+++ src/components/Hrishikesh.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hrishikesh = () => {
+  return (
+    <div>
+      <h1>Hrishikesh M. B.</h1>
+      <h2>Favourite Langauge : Pyhton</h2>
+    </div>
+  );
+};
+export default Hrishikesh;
