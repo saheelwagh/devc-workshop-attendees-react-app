37abe53856a43927d61177a6a7fccebe2b61e20b
adgai19 <gaitondeaditya@gmail.com>
2019-10-06 13:02:17 +0530

    Added Adgai.js created Adgai.js and added component with my name-Aditya Gaitonde and Language-Javascript

---
+++ src/components/Adgai.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Adgai = () => {
+  return (
+    <div>
+      <h1>Aditya Gaitonde</h1>
+      <h2>Favourite Langauge : Python</h2>
+    </div>
+  );
+};
+export default Adgai;
